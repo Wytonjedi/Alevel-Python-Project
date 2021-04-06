d1c7456c0af34ec7f7382f42ab74c09438726fde
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 17:44:53 +0100

    WytonJedi's Commitment

--- Main Python Project/Menu.py
+++ Main Python Project/Menu.py
@@ -1,87 +1,89 @@
+from tkinter import *
 import tkinter as tk
 from Caesar import caesar
 from Vigenere import vigenere
 from Symmetrical_keys import key
 from Rotor_encryption import rotor
 
-
 class menu:
     def __init__(self):
-        # declaration of self variables
+        self.root = tk.Tk()
 
         self.option = ""
-        self.type = ""
-        self.window = tk.Tk()
-        self.window.title("Main Menu")
-        self.frame1 = tk.Frame(self.window)
-        self.frame2 = tk.Frame(self.window)
 
-        # Filling frame 1
-        tk.Label(self.frame1, text="Pick an \n encryption method", width=15, height=2).pack(side=tk.TOP)
-        tk.Button(self.frame1, text="caesar", command=self.choose_caesar, width=15, height=1, bd=4).pack()
-        tk.Button(self.frame1, text="vigenere", command=self.choose_vigenere, width=15, height=1, bd=4).pack()
-        tk.Button(self.frame1, text="symmetrical keys", command=self.choose_key, width=15, height=1, bd=4).pack()
-        tk.Button(self.frame1, text="Rotor encryption", command=self.choose_rotor, width=15, height=1, bd=4).pack()
+        self.wrapper1 = LabelFrame(self.root, text="Encode options")
+        self.wrapper2 = LabelFrame(self.root, text="Description")
+        self.wrapper3 = LabelFrame(self.root, text="Options")
 
-        # Filling frame 2
-        tk.Label(self.frame2, text="Description:").pack()
-        self.textbox = tk.Text(self.frame2, state="disabled", width=75, height=15)
-        self.textbox.pack(side=tk.TOP)
-        tk.Button(self.frame2, text="Exit", command=self.window.destroy, width=15, height=1, bd=4, fg="red"
+        # filling wrapper1
+        Button(self.wrapper1, text="caesar", command=self.choose_caesar, width=15, height=1, bd=4).pack()
+        Button(self.wrapper1, text="vigenere", command=self.choose_vigenere, width=15, height=1, bd=4).pack()
+        Button(self.wrapper1, text="symmetrical keys", command=self.choose_key, width=15, height=1, bd=4).pack()
+        Button(self.wrapper1, text="Rotor encryption", command=self.choose_rotor, width=15, height=1, bd=4).pack()
+
+        # filling wrapper2
+        self.description = tk.Text(self.wrapper2, state="disabled", width=75, height=15)
+        self.description.pack()
+
+        # filling wrapper3
+        tk.Button(self.wrapper3, text="Exit", command=self.root.destroy, width=15, height=1, bd=4, fg="red"
                   ).pack(side=tk.LEFT)
-        tk.Button(self.frame2, text="Enter", command=self.enter, width=15, height=1, bd=4).pack(side=tk.RIGHT)
+        tk.Button(self.wrapper3, text="Enter", command=self.enter, width=15, height=1, bd=4).pack(side=tk.RIGHT)
 
-        # putting both frames on screen
-        self.frame1.pack(side=tk.LEFT)
-        self.frame2.pack(side=tk.RIGHT)
-        self.window.mainloop()
+        # putting wrappers onto window
+        self.wrapper1.grid(row=0, column=0, sticky=N+S)
+        self.wrapper2.grid(row=0, column=1)
+        self.wrapper3.grid(row=1, columnspan=2, sticky=E+W)
+
+        self.root.title("Main Menu")
+        self.root.mainloop()
 
     def choose_caesar(self):
-        self.textbox.configure(state="normal")
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert(tk.END,
-                            """Caesar:
-        
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END,
+"""Caesar:
+
 this is one of the first cyphers ever created""")
-        self.textbox.configure(state="disabled")
+        self.description.configure(state="disabled")
         self.option = "c"
 
     def choose_vigenere(self):
-        self.textbox.configure(state="normal")
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert(tk.END,
-                            """Vigenere:
-                            
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END,
+"""Vigenere:
+
 This was developed from the Caesar cypher using a key instead of 
 a stationary shift""")
-        self.textbox.configure(state="disabled")
+        self.description.configure(state="disabled")
         self.option = "v"
 
     def choose_key(self):
-        self.textbox.configure(state="normal")
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert(tk.END,
-                            """Symmetrical Keys:
-        
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END,
+"""Symmetrical Keys:
+
 this is the most commonly used encryption methods used to day """)
-        self.textbox.configure(state="disabled")
+        self.description.configure(state="disabled")
         self.option = "k"
 
     def choose_rotor(self):
-        self.textbox.configure(state="normal")
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert(tk.END,
-                            """Rotor:
-        
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END,
+"""Rotor:
+
 this is a watered down version of the enigma machine used by the NAZIs""")
-        self.textbox.configure(state="disabled")
+        self.description.configure(state="disabled")
 
         self.option = "r"
 
     def back(self):
-        self.textbox.configure(state="normal")
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.configure(state="disabled")
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.configure(state="disabled")
 
         self.option = ""
 
