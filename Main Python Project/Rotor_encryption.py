c0e4488fe46e0284735d12eabb9f024262f83f52
Wytonjedi <Joseph@elliotts.eu>
2021-06-16 09:14:31 +0100

    WytonJedi's Commitment

--- Main Python Project/Rotor_encryption.py
+++ Main Python Project/Rotor_encryption.py
@@ -5,15 +5,16 @@ from Master import master
 
 class rotor(master):
     def __init__(self):
+        # creation of self variables
+        self.rotor_a = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                        "T", "U", "V", "W", "X", "Y", "Z"]
+        self.rotor_b = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                        "T", "U", "V", "W", "X", "Y", "Z"]
         # creation of string variables
-        self.alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                      "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        self.bravo = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                      "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        self.a_var = tk.StringVar()
-        self.a_var.set(self.alpha[0])
-        self.b_var = tk.StringVar()
-        self.b_var.set(self.bravo[0])
+        self.var_a = StringVar()
+        self.var_a.set(self.rotor_a[0])
+        self.var_b = StringVar()
+        self.var_b.set(self.rotor_b[0])
 
         super().__init__()
 
@@ -22,11 +23,11 @@ class rotor(master):
         tk.Button(self.wrapper2, text="decode", command=self.decode, width=self.b_width, height=self.b_height,
                   bd=self.b_b_width).pack(side=LEFT)
         Label(self.wrapper2, text="Rotor 1:").pack(side=LEFT)
-        self.a_offset = tk.OptionMenu(self.wrapper2, self.a_var, *self.alpha)
+        self.a_offset = tk.OptionMenu(self.wrapper2, self.var_a, *self.rotor_a)
         self.a_offset.configure(bd=self.b_b_width)
         self.a_offset.pack(side=LEFT)
         Label(self.wrapper2, text="Rotor 2:").pack(side=LEFT)
-        self.b_offset = tk.OptionMenu(self.wrapper2, self.b_var, *self.bravo)
+        self.b_offset = tk.OptionMenu(self.wrapper2, self.var_b, *self.rotor_b)
         self.b_offset.configure(bd=self.b_b_width)
         self.b_offset.pack(side=LEFT)
         tk.Button(self.wrapper2, text="encode", command=self.encode, width=self.b_width, height=self.b_height,
@@ -37,12 +38,8 @@ class rotor(master):
                               """Help:
     this is the help text!""")
 
-    def get_offset(self):
-        return self.a_var.get(), self.b_var.get()
-
     def encode(self):
-        offset_a, offset_b = self.get_offset()
-
+        pass
 
     def decode(self):
         pass
--- Main Python Project/Testing.py
+++ Main Python Project/Testing.py
@@ -1,17 +1,32 @@
-import smtplib, ssl
+class rotor:
+    def __init__(self):
+        self.plug_amount = 10
+        self.plugs = [["", ""] for i in range(self.plug_amount)]
+        self.rotor_a = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                        "T", "U", "V", "W", "X", "Y", "Z"]
+        self.rotor_b = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                        "T", "U", "V", "W", "X", "Y", "Z"]
 
-smtp_server = "smtp.gmail.com"
-port = 587  # For starttls
-sender_email = "mypythonprojects101@gmail.com"
-password = "Python123"
-context = ssl.create_default_context()
+    def setup_plugboard(self):
+        a = True
+        count = 0
+        while a:
+            if count > self.plug_amount:
+                a = False
+        plug_a = input("Please enter the fist plug letter: ")
+        plug_b = input("Please enter the second plug letter: ")
+        self.plugs[count][0] = plug_a
+        self.plugs[count][1] = plug_b
+        count += 1
 
-try:
-    server = smtplib.SMTP(smtp_server,port)
-    server.starttls(context=context)
-    server.login(sender_email, password)
-    server.sendmail(sender_email, "15elliottj@stpetershuntingdon.org", "This is a test")
-except Exception as e:
-    print(e)
-finally:
-    server.quit()
+    def setup_rotors(self):
+        offset = int(input("Please enter rotor a offset: "))
+        for i in range(offset):
+            hold = self.rotor_a.pop(0)
+            self.rotor_a.append(hold)
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
