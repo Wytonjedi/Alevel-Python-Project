014a46beb87cbb331232bbbe42acaa7203a2415f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-22 16:47:30 +0100

    WytonJedi's Commitment

--- Main Python Project/Testing.py
+++ Main Python Project/Testing.py
@@ -1,13 +1,11 @@
 import tkinter as tk
 import json
-from Caesar import caesar
-from Vigenere import vigenere
-from Symmetrical_keys import key_sym
-from Rotor_encryption import rotor
+
 
 class menu:
-    root = tk.TK()
+    root = tk.Tk()
     name = "Main Menu"
+    option = ""
 
     @classmethod
     def DESTROY(cls):
@@ -16,10 +14,87 @@ class menu:
     @classmethod
     def JSON(cls):
         with open("package.json") as file:
-            cls.descriptions = json.load(file)
+            j_file = json.load(file)
+            cls.button_vars = j_file["button_vars"]
+            cls.text_vars = j_file["text_vars"]
+            cls.description = j_file["description"]
 
     def __init__(self):
+        self.JSON()  # loading of JSON file
+
+        # creation of root wrappers
+        self.root.name = self.name
+        self.wrapper1 = tk.LabelFrame(self.root, text="Encode options")
+        self.wrapper2 = tk.LabelFrame(self.root, text="Description")
+        self.wrapper3 = tk.LabelFrame(self.root, text="Options")
+        self.root.protocol('WM_DELETE_WINDOW', self.DESTROY)
+
+        # filling of wrapper 1
+        self.buttonC = tk.Button(self.wrapper1, text="caesar", command=self.choose_caesar,
+                                 width=self.button_vars["width"], height=self.button_vars["height"],
+                                 bd=self.button_vars["border"]).pack()
+        self.buttonV = tk.Button(self.wrapper1, text="Viginere", command=self.choose_vigenere,
+                                 width=self.button_vars["width"], height=self.button_vars["height"],
+                                 bd=self.button_vars["border"]).pack()
+        self.buttonK = tk.Button(self.wrapper1, text="Sym Key", command=self.choose_sym_key,
+                                 width=self.button_vars["width"], height=self.button_vars["height"],
+                                 bd=self.button_vars["border"]).pack()
+        self.buttonR = tk.Button(self.wrapper1, text="Rotor", command=self.choose_rotor,
+                                 width=self.button_vars["width"], height=self.button_vars["height"],
+                                 bd=self.button_vars["border"]).pack()
+
+        # filling of wrapper 2
+        self.description = tk.Text(self.wrapper2, state="disabled", width=self.text_vars["width"],
+                                   height=self.text_vars["height"])
+        self.description.pack(expand=True)
+
+        # filling of wrapper 3
+        self.exit = tk.Button(self.wrapper3, text="Exit", command=self.root.destroy, width=15, height=1, bd=4, fg="red"
+                              ).pack(side=tk.LEFT)
+        self.enter = tk.Button(self.wrapper3, text="Enter", command=self.enter, width=15, height=1, bd=4).pack(
+            side=tk.RIGHT)
+        self.help = tk.Button(self.wrapper3, text="Help", command=self.help, width=15, height=1, bd=4).pack(
+            side=tk.RIGHT)
+
+        # packing wrappers to screens
+        self.wrapper1.pack(fill="y", side="left")
+        self.wrapper2.pack(fill="both", expand=True)
+        self.wrapper3.pack(fill="both", side="bottom")
+        self.root.mainloop()
+
+    def choose_caesar(self):
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END, self.description["caesar"])
+        self.description.configure(state="disabled")
+        self.option = "c"
+
+    def choose_vigenere(self):
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END, self.description["vigenere"])
+        self.description.configure(state="disabled")
+        self.option = "c"
+
+    def choose_sym_key(self):
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END, self.description["sys_key"])
+        self.description.configure(state="disabled")
+        self.option = "c"
+
+    def choose_rotor(self):
+        self.description.configure(state="normal")
+        self.description.delete("1.0", tk.END)
+        self.description.insert(tk.END, self.description["rotor"])
+        self.description.configure(state="disabled")
+        self.option = "c"
+
+    def enter(self):
+        pass
+
+    def help(self):
         pass
 
 
-
+menu1 = menu()
